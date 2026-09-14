abf82459b9b01047611489797dc3634893ed1660
rahul01112007-sudo <rahul01112007@gmail.com>
2026-09-14 15:09:59 +0530

    Add litertlm-android dependency to build.gradle.kts

--- extracted/app/build.gradle.kts
+++ extracted/app/build.gradle.kts
@@ -70,6 +70,7 @@ googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.W
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
 dependencies {
+implementation("com.google.ai.edge.litertlm:litertlm-android:0.13.1")
   implementation(platform(libs.androidx.compose.bom))
   implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
